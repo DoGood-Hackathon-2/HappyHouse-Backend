1942f9219ccbe011475bf256584d5ef85788d414
ayoung0073 <ayong0310@naver.com>
2021-08-14 16:53:38 +0900

    add: Family, Member 엔티티 클래스, 레포지토리 생성

---
+++ src/main/java/com/example/happyhousebackend/domain/family/FamilyRepository.java
@@ -0,0 +1,6 @@
+package com.example.happyhousebackend.domain.family;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FamilyRepository extends JpaRepository<Family, Long> {
+}
