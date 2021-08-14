0716eaf9e8a6169e609621765f7bc625c9a049c8
JuHyun419 <zzang9ha@naver.com>
2021-08-15 03:10:42 +0900

    add: 마이페이지 dto

--- src/main/java/com/example/happyhousebackend/domain/member/controller/dto/MyMemberRequestDto.java
+++ src/main/java/com/example/happyhousebackend/domain/member/controller/dto/MyMemberRequestDto.java
@@ -1,21 +1,16 @@
 package com.example.happyhousebackend.domain.member.controller.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-@Builder
-@ToString
-public class MemberList {
+public class MyMemberRequestDto {
 
     private Long id;
-    private String nickname;
-    private String image;
+    private String name;
 }
