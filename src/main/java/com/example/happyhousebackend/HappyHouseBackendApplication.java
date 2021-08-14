1736d48b0bf1c592d1b2ff85eb9132b4ab7bb934
JuHyun419 <zzang9ha@naver.com>
2021-08-14 12:17:12 +0900

    initial commit

---
+++ src/main/java/com/example/happyhousebackend/HappyHouseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.happyhousebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyHouseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyHouseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/happyhousebackend/HappyHouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.happyhousebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyHouseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
