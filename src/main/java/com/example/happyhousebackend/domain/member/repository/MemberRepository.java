309af1530bd807caa37b15ddc36359e829479aff
JuHyun419 <zzang9ha@naver.com>
2021-09-20 17:27:20 +0900

    feat: OAuth2 메소드 추가

--- src/main/java/com/example/happyhousebackend/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/happyhousebackend/domain/member/repository/MemberRepository.java
@@ -4,9 +4,12 @@ import com.example.happyhousebackend.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    public List<Member> findAllByFamilyId(Long familyId);
+    List<Member> findAllByFamilyId(Long familyId);
+
+    Optional<Member> findByEmail(String email);
 
 }
