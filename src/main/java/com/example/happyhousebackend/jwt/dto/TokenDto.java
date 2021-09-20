bf4b900d1805822a5152374120ac5c4edfa84922
JuHyun419 <zzang9ha@naver.com>
2021-09-20 17:34:55 +0900

    feat: Jwt TokenDto 추가

---
+++ src/main/java/com/example/happyhousebackend/jwt/dto/TokenDto.java
@@ -0,0 +1,15 @@
+package com.example.happyhousebackend.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDto {
+
+    private String accessToken;
+
+    private String refreshToken;
+}
